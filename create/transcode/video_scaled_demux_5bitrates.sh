dbe784b7ad416ae3188c3b48462705efbf433462
Greg Rutz <g.rutz@cablelabs.com>
2015-01-05 11:55:59 -0700

    Name transcoded files as audio* and video*

--- create/transcode/video_scaled_demux_5bitrates.sh
+++ create/transcode/video_scaled_demux_5bitrates.sh
@@ -25,7 +25,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-avconv=/home/grutz/Projects/CableLabs/libav/avconv
+avconv=avconv
 
 function usage {
   echo ""
@@ -47,17 +47,17 @@ fi
 
 mkdir -p $2
 
-$avconv -threads auto -i $1 -vf "scale=w=512:h=288" -map_chapters -1 -an -codec:v libx264 -profile:v main -level 21 -b:v 360k $2/abr_512x288_h264-360Kb.mp4
+$avconv -threads auto -i $1 -vf "scale=w=512:h=288" -map_chapters -1 -an -codec:v libx264 -profile:v main -level 21 -b:v 360k $2/video_512x288_h264-360Kb.mp4
 
-$avconv -threads auto -i $1 -vf "scale=w=704:h=396" -map_chapters -1 -an -codec:v libx264 -profile:v main -level 30 -b:v 620k $2/abr_704x396_h264-620Kb.mp4
+$avconv -threads auto -i $1 -vf "scale=w=704:h=396" -map_chapters -1 -an -codec:v libx264 -profile:v main -level 30 -b:v 620k $2/video_704x396_h264-620Kb.mp4
 
-$avconv -threads auto -i $1 -vf "scale=w=896:h=504" -map_chapters -1 -an -codec:v libx264 -profile:v high -level 31 -b:v 1340k $2/abr_896x504_h264-1340Kb.mp4
+$avconv -threads auto -i $1 -vf "scale=w=896:h=504" -map_chapters -1 -an -codec:v libx264 -profile:v high -level 31 -b:v 1340k $2/video_896x504_h264-1340Kb.mp4
 
-$avconv -threads auto -i $1 -vf "scale=w=1280:h=720" -map_chapters -1 -an -codec:v libx264 -profile:v high -level 32 -b:v 2500k $2/abr_1280x720_h264-2500Kb.mp4
+$avconv -threads auto -i $1 -vf "scale=w=1280:h=720" -map_chapters -1 -an -codec:v libx264 -profile:v high -level 32 -b:v 2500k $2/video_1280x720_h264-2500Kb.mp4
 
-$avconv -threads auto -i $1 -vf "scale=w=1920:h=1080" -map_chapters -1 -sn -an -codec:v libx264 -profile:v high -level 40 -b:v 4500k  $2/abr_1920x1080_h264-4500Kb.mp4
+$avconv -threads auto -i $1 -vf "scale=w=1920:h=1080" -map_chapters -1 -sn -an -codec:v libx264 -profile:v high -level 40 -b:v 4500k  $2/video_1920x1080_h264-4500Kb.mp4
 
-$avconv -threads auto -i $1 -map_chapters -1 -vn -codec:a libfdk_aac -profile:a aac_low -b:a 128k $2/abr_aac-lc_128k.mp4
+$avconv -threads auto -i $1 -map_chapters -1 -vn -codec:a libfdk_aac -profile:a aac_low -b:a 128k $2/audio_aac-lc_128k.mp4
 
-$avconv -threads auto -i $1 -map_chapters -1 -vn -codec:a libfdk_aac -profile:a aac_low -b:a 192k $2/abr_aac-lc_192k.mp4
+$avconv -threads auto -i $1 -map_chapters -1 -vn -codec:a libfdk_aac -profile:a aac_low -b:a 192k $2/audio_aac-lc_192k.mp4
 
